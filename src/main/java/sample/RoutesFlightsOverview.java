f9edb50b1ebccd33aa241e08f8398ef10846e6ee
Gordeev Pavel <pvgord@icloud.com>
2017-11-25 18:46:41 +0400

    fixed bugs with buttons

--- src/main/java/sample/RoutesFlightsOverview.java
+++ src/main/java/sample/RoutesFlightsOverview.java
@@ -10,13 +10,13 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class RoutesFlightsOverview {
+public class RoutesFlightsOverview{
 
-    private static final String EDIT_ROUTE_WINDOW = "Edit a route";
-    private static final String ADD_ROUTE_WINDOW = "Add a route";
-    private static final String EDIT_FLIGHT_WINDOW = "Edit a flight";
-    private static final String ADD_FLIGHT_WINDOW = "Edit a route";
-    private static final String SEARCH_ROUTE_WINDOW = "Search a route";
+    private static final String EDIT_ROUTE_WINDOW    = "Edit a route";
+    private static final String ADD_ROUTE_WINDOW     = "Add a route";
+    private static final String EDIT_FLIGHT_WINDOW   = "Edit a flight";
+    private static final String ADD_FLIGHT_WINDOW    = "Edit a route";
+    private static final String SEARCH_ROUTE_WINDOW  = "Search a route";
     private static final String SEARCH_FLIGHT_WINDOW = "Search a flight";
 
     public Button addRouteButton;
@@ -30,155 +30,153 @@ public class RoutesFlightsOverview {
     public Button detailFlightButton;
 
 
-    public void handleAddRouteButton(ActionEvent actionEvent) {
+    public void handleAddRouteButton( ActionEvent actionEvent ){
         Parent addRouteWindow = null;
-        Stage oldStage = (Stage) ((Parent) actionEvent.getSource()).getScene().getWindow();
-        try {
-            addRouteWindow = FXMLLoader.load(getClass().getResource( "/fxml/AddRoutesOverview.fxml" ) );
+        Stage  oldStage       = ( Stage ) ( ( Parent ) actionEvent.getSource() ).getScene().getWindow();
+        try{
+            addRouteWindow = FXMLLoader.load( getClass().getResource( "/fxml/AddRoutesOverview.fxml" ) );
 
-            Scene scene = new Scene(addRouteWindow);
+            Scene scene = new Scene( addRouteWindow );
             Stage popUp = new Stage();
 
-            popUp.initModality(Modality.APPLICATION_MODAL);
-            popUp.initOwner(oldStage);
+            popUp.initModality( Modality.APPLICATION_MODAL );
+            popUp.initOwner( oldStage );
 
-            popUp.setTitle(ADD_ROUTE_WINDOW);
-            popUp.setScene(scene);
-            popUp.setResizable(false);
+            popUp.setTitle( ADD_ROUTE_WINDOW );
+            popUp.setScene( scene );
+            popUp.setResizable( false );
 
-            oldStage.setOpacity(0.9);
+            oldStage.setOpacity( 0.9 );
             popUp.showAndWait();
-            oldStage.setOpacity(1);
+            oldStage.setOpacity( 1 );
 
-        } catch (IOException e) {
+        }catch( IOException e ){
             e.printStackTrace();
         }
     }
 
-    public void handleEditRouteButton(ActionEvent actionEvent) {
+    public void handleEditRouteButton( ActionEvent actionEvent ){
         Parent editRouteWindow = null;
-        Stage oldStage = (Stage) ((Parent) actionEvent.getSource()).getScene().getWindow();
-        try {
-            editRouteWindow = FXMLLoader.load(getClass().getResource( "../../resources/fxml/EditRoutesOverview.fxml" ) );
+        Stage  oldStage        = ( Stage ) ( ( Parent ) actionEvent.getSource() ).getScene().getWindow();
+        try{
+            editRouteWindow = FXMLLoader.load( getClass().getResource( "/fxml/EditRoutesOverview.fxml" ) );
 
-            Scene scene = new Scene(editRouteWindow);
+            Scene scene = new Scene( editRouteWindow );
             Stage popUp = new Stage();
 
-            popUp.initModality(Modality.APPLICATION_MODAL);
-            popUp.initOwner(oldStage);
+            popUp.initModality( Modality.APPLICATION_MODAL );
+            popUp.initOwner( oldStage );
 
-            popUp.setTitle(EDIT_ROUTE_WINDOW);
-            popUp.setScene(scene);
-            popUp.setResizable(false);
+            popUp.setTitle( EDIT_ROUTE_WINDOW );
+            popUp.setScene( scene );
+            popUp.setResizable( false );
 
-            oldStage.setOpacity(0.9);
+            oldStage.setOpacity( 0.9 );
             popUp.showAndWait();
-            oldStage.setOpacity(1);
+            oldStage.setOpacity( 1 );
 
-        } catch (IOException e) {
+        }catch( IOException e ){
             e.printStackTrace();
         }
     }
 
-    public void handleAddFlightButton(ActionEvent actionEvent) {
+    public void handleAddFlightButton( ActionEvent actionEvent ){
         Parent addFlightWindow = null;
-        Stage oldStage = (Stage) ((Parent) actionEvent.getSource()).getScene().getWindow();
-        try {
-            addFlightWindow = FXMLLoader.load(getClass().getResource( "../../resources/fxml/AddFlightsOverview.fxml" ) );
+        Stage  oldStage        = ( Stage ) ( ( Parent ) actionEvent.getSource() ).getScene().getWindow();
+        try{
+            addFlightWindow = FXMLLoader.load( getClass().getResource( "/fxml/AddFlightsOverview.fxml" ) );
 
-            Scene scene = new Scene(addFlightWindow);
+            Scene scene = new Scene( addFlightWindow );
             Stage popUp = new Stage();
 
-            popUp.initModality(Modality.APPLICATION_MODAL);
-            popUp.initOwner(oldStage);
+            popUp.initModality( Modality.APPLICATION_MODAL );
+            popUp.initOwner( oldStage );
 
-            popUp.setTitle(ADD_FLIGHT_WINDOW);
-            popUp.setScene(scene);
-            popUp.setResizable(false);
+            popUp.setTitle( ADD_FLIGHT_WINDOW );
+            popUp.setScene( scene );
+            popUp.setResizable( false );
 
-            oldStage.setOpacity(0.9);
+            oldStage.setOpacity( 0.9 );
             popUp.showAndWait();
-            oldStage.setOpacity(1);
+            oldStage.setOpacity( 1 );
 
-        } catch (IOException e) {
+        }catch( IOException e ){
             e.printStackTrace();
         }
     }
 
-    public void handleEditFlightButton(ActionEvent actionEvent) {
+    public void handleEditFlightButton( ActionEvent actionEvent ){
         Parent editFlightWindow = null;
-        Stage oldStage = (Stage) ((Parent) actionEvent.getSource()).getScene().getWindow();
-        try {
-            editFlightWindow = FXMLLoader.load(getClass().getResource(
-                    "../../resources/fxml/EditFlightsOverview.fxml" ) );
+        Stage  oldStage         = ( Stage ) ( ( Parent ) actionEvent.getSource() ).getScene().getWindow();
+        try{
+            editFlightWindow = FXMLLoader.load( getClass().getResource( "/resources/fxml/EditFlightsOverview.fxml" ) );
 
-            Scene scene = new Scene(editFlightWindow);
+            Scene scene = new Scene( editFlightWindow );
             Stage popUp = new Stage();
 
-            popUp.initModality(Modality.APPLICATION_MODAL);
-            popUp.initOwner(oldStage);
+            popUp.initModality( Modality.APPLICATION_MODAL );
+            popUp.initOwner( oldStage );
 
-            popUp.setTitle(EDIT_FLIGHT_WINDOW);
-            popUp.setScene(scene);
-            popUp.setResizable(false);
+            popUp.setTitle( EDIT_FLIGHT_WINDOW );
+            popUp.setScene( scene );
+            popUp.setResizable( false );
 
-            oldStage.setOpacity(0.9);
+            oldStage.setOpacity( 0.9 );
             popUp.showAndWait();
-            oldStage.setOpacity(1);
+            oldStage.setOpacity( 1 );
 
-        } catch (IOException e) {
+        }catch( IOException e ){
             e.printStackTrace();
         }
     }
 
-    public void handleSearchRouteButton(ActionEvent actionEvent) {
+    public void handleSearchRouteButton( ActionEvent actionEvent ){
         Parent editFlightWindow = null;
-        Stage oldStage = (Stage) ((Parent) actionEvent.getSource()).getScene().getWindow();
-        try {
-            editFlightWindow = FXMLLoader.load(getClass().getResource(
-                    "../../resources/fxml/SearchRoutesOverview.fxml" ) );
+        Stage  oldStage         = ( Stage ) ( ( Parent ) actionEvent.getSource() ).getScene().getWindow();
+        try{
+            editFlightWindow = FXMLLoader.load( getClass().getResource( "/resources/fxml/SearchRoutesOverview.fxml" ) );
 
-            Scene scene = new Scene(editFlightWindow);
+            Scene scene = new Scene( editFlightWindow );
             Stage popUp = new Stage();
 
-            popUp.initModality(Modality.APPLICATION_MODAL);
-            popUp.initOwner(oldStage);
+            popUp.initModality( Modality.APPLICATION_MODAL );
+            popUp.initOwner( oldStage );
 
-            popUp.setTitle(SEARCH_ROUTE_WINDOW);
-            popUp.setScene(scene);
-            popUp.setResizable(false);
+            popUp.setTitle( SEARCH_ROUTE_WINDOW );
+            popUp.setScene( scene );
+            popUp.setResizable( false );
 
-            oldStage.setOpacity(0.9);
+            oldStage.setOpacity( 0.9 );
             popUp.showAndWait();
-            oldStage.setOpacity(1);
+            oldStage.setOpacity( 1 );
 
-        } catch (IOException e) {
+        }catch( IOException e ){
             e.printStackTrace();
         }
     }
 
-    public void handleSearchFlightButton(ActionEvent actionEvent) {
+    public void handleSearchFlightButton( ActionEvent actionEvent ){
         Parent editFlightWindow = null;
-        Stage oldStage = (Stage) ((Parent) actionEvent.getSource()).getScene().getWindow();
-        try {
-            editFlightWindow = FXMLLoader.load(getClass().getResource(
-                    "../../resources/fxml/SearchFlightsOverview.fxml" ) );
+        Stage  oldStage         = ( Stage ) ( ( Parent ) actionEvent.getSource() ).getScene().getWindow();
+        try{
+            editFlightWindow =
+                    FXMLLoader.load( getClass().getResource( "/resources/fxml/SearchFlightsOverview.fxml" ) );
 
-            Scene scene = new Scene(editFlightWindow);
+            Scene scene = new Scene( editFlightWindow );
             Stage popUp = new Stage();
 
-            popUp.initModality(Modality.APPLICATION_MODAL);
-            popUp.initOwner(oldStage);
+            popUp.initModality( Modality.APPLICATION_MODAL );
+            popUp.initOwner( oldStage );
 
-            popUp.setTitle(SEARCH_FLIGHT_WINDOW);
-            popUp.setScene(scene);
-            popUp.setResizable(false);
+            popUp.setTitle( SEARCH_FLIGHT_WINDOW );
+            popUp.setScene( scene );
+            popUp.setResizable( false );
 
-            oldStage.setOpacity(0.9);
+            oldStage.setOpacity( 0.9 );
             popUp.showAndWait();
-            oldStage.setOpacity(1);
+            oldStage.setOpacity( 1 );
 
-        } catch (IOException e) {
+        }catch( IOException e ){
             e.printStackTrace();
         }
     }
