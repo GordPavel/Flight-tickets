17bd88bf2f25329a05b449011557700a52bf7797
teomant <teomant@bk.ru>
2017-11-27 20:32:29 +0400

    better search

--- src/main/java/sample/SearchFlightsOverviewController.java
+++ src/main/java/sample/SearchFlightsOverviewController.java
@@ -51,6 +51,7 @@ public class SearchFlightsOverviewController{
         Stage stage = (Stage) ((Parent) event.getSource()).getScene().getWindow();
         stage.close();
     }
+
     public void handleCancelAction(ActionEvent actionEvent) {
         closeWindow(actionEvent);
 
@@ -61,98 +62,82 @@ public class SearchFlightsOverviewController{
         departureDateToDatePicker.setValue(null);
         arrivingDateToDatePicker.setValue(null);
         arrivingDateFromDatePicker.setValue(null);
+        numberField.clear();
+        planeID.clear();
+        arrivingDateFromTextField.clear();
+        arrivingDateToTextField.clear();
+        departureDateFromTextField.clear();
+        departureDateToTextField.clear();
+        flightTimeFromTextField.clear();
+        flightTimeToTextField.clear();
+
     }
 
     public void handleSearchAction(ActionEvent event){
 
-        DateFormat format = new SimpleDateFormat("hh:mm");
+        DateFormat format = new SimpleDateFormat("dd.MM.yyyy hh:mm");
 
         Date dfDate=null;
 
         if (departureDateFromDatePicker.getValue()!=null)
         {
-            LocalDate depFromDate = departureDateFromDatePicker.getValue();
-            Instant dfinstant = Instant.from(depFromDate.atStartOfDay(ZoneId.systemDefault()));
-            dfDate = Date.from(dfinstant);
-            Date dfTime = new Date();
             try {
-                dfTime = format.parse(departureDateFromTextField.getText());
+                dfDate = format.parse(departureDateFromDatePicker.getEditor().getText()+" "+departureDateFromTextField.getText());
             }
             catch (ParseException e)
             {
 
             }
-            dfDate.setHours(dfTime.getHours());
-            dfDate.setMinutes(dfTime.getMinutes());
-            dfDate.setSeconds(dfTime.getSeconds());
         }
 
         Date dtDate=null;
 
         if (departureDateToDatePicker.getValue()!=null)
         {
-            LocalDate depToDate = departureDateToDatePicker.getValue();
-            Instant dtinstant = Instant.from(depToDate.atStartOfDay(ZoneId.systemDefault()));
-            dtDate = Date.from(dtinstant);
-            Date dtTime = new Date();
             try {
-                dtTime = format.parse(departureDateToTextField.getText());
+                dtDate = format.parse(departureDateToDatePicker.getEditor().getText()+" "+departureDateToTextField.getText());
             }
             catch (ParseException e)
             {
 
             }
-            dtDate.setHours(dtTime.getHours());
-            dtDate.setMinutes(dtTime.getMinutes());
-            dtDate.setSeconds(dtTime.getSeconds());
         }
-
 
         Date afDate=null;
 
         if (arrivingDateFromDatePicker.getValue()!=null)
         {
-            LocalDate arrFromDate = arrivingDateFromDatePicker.getValue();
-            Instant afinstant = Instant.from(arrFromDate.atStartOfDay(ZoneId.systemDefault()));
-            afDate = Date.from(afinstant);
-            Date afTime = new Date();
             try {
-                afTime = format.parse(arrivingDateFromTextField.getText());
+                afDate = format.parse(arrivingDateFromDatePicker.getEditor().getText()+" "+arrivingDateFromTextField.getText());
             }
             catch (ParseException e)
             {
 
             }
-            afDate.setHours(afTime.getHours());
-            afDate.setMinutes(afTime.getMinutes());
-            afDate.setSeconds(afTime.getSeconds());
         }
 
         Date atDate=null;
 
         if (arrivingDateToDatePicker.getValue()!=null)
         {
-            LocalDate arrToDate = arrivingDateToDatePicker.getValue();
-            Instant atinstant = Instant.from(arrToDate.atStartOfDay(ZoneId.systemDefault()));
-            atDate = Date.from(atinstant);
-            Date atTime = new Date();
             try {
-                atTime = format.parse(arrivingDateToTextField.getText());
+                atDate = format.parse(arrivingDateToDatePicker.getEditor().getText()+" "+arrivingDateToTextField.getText());
             }
             catch (ParseException e)
             {
 
             }
-            atDate.setHours(atTime.getHours());
-            atDate.setMinutes(atTime.getMinutes());
-            atDate.setSeconds(atTime.getSeconds());
         }
 
         System.out.println(dfDate);
-        System.out.println(dtDate);
+        System.out.println(departureDateFromDatePicker.getEditor().getText()+" "+departureDateFromTextField.getText());
         System.out.println(Main.getEngine().searchFlight(numberField.getText(),null,"","", planeID.getText(),
             dfDate, dtDate, afDate, atDate));
 
 
     }
+
+
+
+
 }
