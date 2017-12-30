9bbaac70c8840d3fe39b3e0b380567a8f48297a5
Gord Pavel <pvgord@icloud.com>
2017-12-30 16:44:21 +0400

    little fixes

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -21,8 +21,7 @@ public class Main extends Application{
 
     @Override
     public void start( Stage primaryStage ) throws Exception{
-        FXMLLoader                      loader     =
-                new FXMLLoader( getClass().getResource( "/fxml/RoutesFlightsOverview.fxml" ) );
+        FXMLLoader loader = new FXMLLoader( getClass().getResource( "/fxml/RoutesFlightsOverview.fxml" ) );
         RoutesFlightsOverviewController controller = new RoutesFlightsOverviewController( primaryStage );
         loader.setController( controller );
         primaryStage.setTitle( "Information system about flights and routes" );
